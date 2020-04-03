08300de4e5e701d86e3e91062dd4eeefe98fcbd8
Stanislav Hrechykhin <stanislav.hrechykhin@gmail.com>
2020-04-03 19:33:24 +0300

    Add environments service and models.

---
+++ Models/Environments/EnvironmentContentModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Postman_API.Models.Environments
+{
+    class EnvironmentContentModel
+    {
+        public Environment environment { get; set; }
+    }
+
+    public class Environment
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public List<Value> values { get; set; }
+    }
+
+    public class Value
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+        public string type { get; set; }
+        public bool enabled { get; set; }
+        public bool hovered { get; set; }
+    }
+}
---
+++ Services/EnvironmentService.cs
@@ -0,0 +1,37 @@
+﻿using Postman_API.Models.Environments;
+using Newtonsoft.Json;
+
+namespace Postman_API.Services
+{
+    class EnvironmentService : WebClientRequests
+    {
+        public EnvironmentService() : base() { }
+
+        public EnvironmentListInfoModel GetAllCollections()
+        {
+            return Get<EnvironmentListInfoModel>(Constants.baseUri + "/environments");
+        }
+
+        public EnvironmentContentModel GetSingleCollection(string uid)
+        {
+            return Get<EnvironmentContentModel>(Constants.baseUri + "/environments" + $"/{uid}");
+        }
+
+        public EnvironmentInfoModel CreateCollection(EnvironmentContentModel content)
+        {
+            var serializedContend = JsonConvert.SerializeObject(content);
+            return Post<EnvironmentInfoModel>(Constants.baseUri + "/environments", serializedContend);
+        }
+
+        public EnvironmentInfoModel UpdateCollection(EnvironmentContentModel content, string uid)
+        {
+            var serializedContend = JsonConvert.SerializeObject(content);
+            return Put<EnvironmentInfoModel>(Constants.baseUri + "/environments" + $"/{uid}", serializedContend);
+        }
+
+        public EnvironmentInfoModel DeleteCollection(string uid)
+        {
+            return Delete<EnvironmentInfoModel>(Constants.baseUri + "/environments" + $"/{uid}");
+        }
+    }
+}
